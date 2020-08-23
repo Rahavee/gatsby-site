46c9fa7b1ee7f0c7b9f4da579fdc42cc24e35868
Rahavee <rmanian@uvm.edu>
2020-08-23 13:17:43 -0400

    Loaded Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
-import React from "react"
+import React from 'react';
+import splash from '../Images/splash.jpg';
+import {makeStyles} from "@material-ui/core/styles";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+const useStyles = makeStyles({
+  mainPage: {
+    backgroundImage: `url(${splash})`,
+    width: "100%",
+    height:"100vh",
+    backgroundPosition:"center",
+    backgroundSize:"cover",
+  }
+});
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className="App">
+      <CssBaseline/>
+      <div className={classes.mainPage}>
+        <div>Hi Dude!</div>
+
+      </div>
+
+
+    </div>
+  );
 }
+
+export default App;
